6586614214bc929342726ca32806c6051c3a3fe1
Arsh Rai <arshabhirai@gmail.com>
2020-01-02 16:00:12 -0500

    Added test blog

---
+++ src/pages/articles/2019-10-18---Recapping-Day1-SamshingConf2019/index.md
@@ -0,0 +1,25 @@
+---
+title: Recapping Day 1 of SmashingConf Toronto
+date: "2020-01-18T16:51:00.000Z"
+layout: post
+draft: false
+path: "/posts/Recapping-Day1-SamshingConf2019/"
+category: "COnference"
+tags:
+  - "SamshingConf"
+  - "Web Development"
+description: "Day 1 of Smashing Conference Toronto 2019"
+---
+
+I was stoked to attend SmashingConf in Toronto this year. SmashingConf is the place to be if you want to learn about front-end and UX development. My colleague Blaine Hussey and I were so excited that we showed up at 8:00 a.m. and waited anxiously for the doors to open.
+
+On the first day, seven speakers delivered compelling talks about everything from animation and front-end code architecture to inclusive design and digital policies. I’ve summarized what they talked about here, and written more over on Medium.
+
+Let’s Build a Design System: Brad Frost, author of the book Atomic Design and co-host of the Style Guides podcast, kicked things off by talking about how to build a design system. He shared some great ideas about code architecture, modularity and reuse of components; demonstrating key concepts with a pair of front-end development tools called io and Story UI. In my recent experience re-designing Solace’s documentation site, it was critical to design and code through the lens of the software we use, which also included third-party software we wanted to integrate.
+Let’s Build a Vue App from Scratch: We then heard from Sarah Drasner, who’s the head of developer experience at Netlify and a member of the team behind Vue.js, an open source javascript library for building user interfaces and single-page applications. She showed us how to use Vue to build a functional application with routing, server-side rendering, and animations using Nuxt.js and her Vue snippet extensions. I was intrigued by how she used her custom Vue snippets and other tools such as CSS Grid generator, that she had developed herself, to speed up her workflow. Very cool!
+JAMStack: Silly Name, Serious Stuff: Then Phil Hawksworth, head of developer relations for Netlify, cleared up what JAMStack means (Javascript + APIs + pre-rendered Markup), and demonstrated how WebHooks and APIs can be used to trigger events, generate notifications and create and automate the development environment. His key message was that you should simplify the technology stack, decouple sites from deployment environments and automate as much as you can.
+Building Bridges, Not Walls: Seasoned UX consultant Jenny Shen gave a fascinating talk about how cultural differences do – or should, at least – influence product and interface design. She made it clear that it’s no longer enough to simply translate text on the page – to increase trust and engagement, designers must understand and factor for the wide range of cultural, habitual and other social differences across countries.
+Let’s Create a Web Animation from Scratch: Illustrator and animator Chris Gannon walked us through the process of creating an animation with SVG code. It was fascinating to see the decisions he made as he coded and how even the smallest detail in code can have a significant impact on the output.
+Digital Policy and Standards Rehab Hour: Kristina Podnar, author of The Power of Digital Policy, explained how the often overlooked matter of digital policy and standards actually plays a vital role in creating a good website. She talked about some common challenges and how to use policies and standards to enhance website health.
+Authentic Digital Design by the Numbers: Last but not least, Steven Hoober (who literally wrote the book on mobile design, i.e. O’Reilly’s Designing Mobile Interfaces) detailed the unique usability challenges faced when designing for mobile devices, such as the fact that people don’t start at the top of mobile screens, but in the middle.
+It was an informative day, and if you find these topics as fascinating as I did, I think you’ll get a lot out of my detailed description of their presentations over on Medium.
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,43 +4,154 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
 
-
-
 class portfolioRoute extends React.Component {
   render() {
-    return  (
-  <Layout>
-    <div>
-    <Sidebar {...this.props} />
-    <div className="content">
-    <div className="content__inner">
-      <div className="page">
-    <h1 className="page__title">About me</h1>
-    <p>I am writer and a designer based in Ottawa, Canada. I enjoy taking complex ideas from brightest minds and presenting them in the most elegant way possible. I live by the principle that age is just a number and learning is never-ending. I love to inspire and be inspired.</p>    
-      <h2>Random facts</h2>
-      <p> I play guitar in my free time. If have a minute, listen to my guitar rants on my instagram page.</p>
-      <p> I am obsessed about Darjeeling tea and often find myself brewing, tasting, and always seeking that perfect muscatel flavor. Check out my quick tutorial about brewing a perfect Darjeeling tea. </p>
-    <h2>Technical Communication</h2>
-    <p>
-    With over seven years of experience writing and designing many types of documentation -- from technical documents through to marketing materials, and  web content -- I know exactly what it takes to simplify complex information into everyday language. I write and design documentation that is easy for the users to follow that enables them to quickly complete their task at hand. Whether that task is to complete a software installation, understand how a feature works, or gather information about a product.</p>
-<p>
-Good communication requires intelligent content that enables the audience to engage with your product. I can help your software development team write engaging content and design documentation that users can easily access and consume using their mobile phones, tablets, or computers.  With information available at anytime—either as an online help, a video tutorial, or a user guide—your customers can achieve their goals and enjoy your products without calling customer support.</p>
-<p>
-I can develop conceptual information, task oriented walkthrough, online help, instructional videos, user guides templates & training materials.
-    </p>
-    <h2>User Experience Design</h2>
-    <p>I have a deep interest in understanding the different ways users discover and process information. </p>
-    <p>
-I keep myself updated with the latest in Information Architecture and UX design through various meetups, books, and online resources. I am proficient at using popular software tools to design illustrations, graphics, workflows, and manipulate images to simplify technical information and enhance users’ understanding of the material.</p>
-    <h2>Web Development</h2>
-    <p>In the ever-changing world of science and technology, I stay plugged-in to modern tools and technologies. I believe every technical communications professional must possess basic coding skills to produce content for various platforms. I continuously educate and train myself in various coding and programming languages. </p>
-  </div>
-  </div>
-  </div>
-  </div>
-  </Layout>
-)
-    }
+    return (
+      <Layout>
+        <div>
+          <Sidebar {...this.props} />
+          <div className="content">
+            <div className="content__inner">
+              <div className="page">
+                <h1 className="page__title">About me</h1>
+                <p>
+                  I am writer and a designer based in Ottawa, Canada. I enjoy
+                  taking complex ideas from the brightest minds and presenting
+                  them in the most elegant way possible. I live by the principle
+                  that age is just a number and learning is never-ending. I love
+                  to inspire and be inspired.
+                </p>
+                <svg
+                  id="a68193b5-dc28-47e4-a46a-282d53b01437"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 480 475.13"
+                >
+                  <title>Arsh Rai - Writer, Designer, Developer</title>
+                  <path
+                    d="M242.29,0V235.26H480c0-65-28.13-123.83-70.4-165.62S308,1.53,242.29,0Z"
+                    fill-rule="evenodd"
+                  />
+                  <path
+                    d="M478.38,263.11H234.49L67.29,402.42c20.25,21.66,45.31,38.68,73.48,51.09,29.62,13.9,64.1,21.62,100,21.62,64.12,0,121.91-24.74,164.11-66.53a230.27,230.27,0,0,0,73.47-145.49Z"
+                    fill-rule="evenodd"
+                  />
+                  <path
+                    d="M214.24,243V1.53C156.33,7.72,104.72,34,67.29,74.28,25.06,116.07,0,173.34,0,236.8c0,29.42,4.69,57.27,15.68,83.58A225.89,225.89,0,0,0,48.5,380.76Z"
+                    fill-rule="evenodd"
+                  />
+                  <text
+                    transform="translate(46.26 191.15)"
+                    font-size="45.46"
+                    fill="#fff"
+                    font-family="MyriadPro-Regular, Myriad Pro"
+                    letter-spacing="-0.02em"
+                  >
+                    w
+                    <tspan x="37.73" y="0" letter-spacing="0em">
+                      r
+                    </tspan>
+                    <tspan x="52.78" y="0" letter-spacing="0em">
+                      i
+                    </tspan>
+                    <tspan x="63.41" y="0" letter-spacing="-0.01em">
+                      t
+                    </tspan>
+                    <tspan x="78.19" y="0" letter-spacing="0em">
+                      er
+                    </tspan>
+                  </text>
+                  <text
+                    transform="translate(207.48 376.63)"
+                    font-size="45.46"
+                    fill="#fff"
+                    font-family="MyriadPro-Regular, Myriad Pro"
+                  >
+                    <tspan letter-spacing="0em">d</tspan>
+                    <tspan x="30.46" y="0" letter-spacing="0em">
+                      esi
+                    </tspan>
+                    <tspan x="81.87" y="0" letter-spacing="-0.01em">
+                      g
+                    </tspan>
+                    <tspan x="107.01" y="0">
+                      ner
+                    </tspan>
+                  </text>
+                  <text
+                    transform="translate(265.39 155.15)"
+                    font-size="45.46"
+                    fill="#fff"
+                    font-family="MyriadPro-Regular, Myriad Pro"
+                  >
+                    <tspan letter-spacing="-0.01em">c</tspan>
+                    <tspan x="25.87" y="0">
+                      oder
+                    </tspan>
+                  </text>
+                </svg>
+                <h2>Random facts</h2>
+                <p>
+                  {' '}
+                  I play guitar during my free time. If you have a minute, listen to my
+                  guitar rants on instagram.
+                </p>
+                <p>
+                  {' '}
+                  I  drink a lot of tea, Darjeeling tea to be precise. At home, I am always brewing, tasting, and seeking that perfect muscatel
+                  flavor. Check out my tutorial on brewing a perfect cup of Darjeeling tea.{' '}
+                </p>
+                <h2>Technical Communication</h2>
+                <p>
+                  With over seven years of experience writing and designing many
+                  types of documentation&mdash;from technical documents through to
+                  marketing materials, and web content&mdash;I know exactly what it
+                  takes to simplify complex information into everyday language.
+                  I write and design documentation that is easy for the users to
+                  follow, and enables them to quickly complete their task at
+                  hand. Whether that task is to complete a software
+                  installation, understand how a feature works, or gather
+                  information about a product.
+                </p>
+                <p>
+                  Good communication requires intelligent content that enables
+                  the audience to engage with your product. I can help your
+                  software development team write engaging content and design
+                  documentation that users can easily access and consume using
+                  their mobile phones, tablets, or computers. With information
+                  available at anytim&mdash;either as an online help, video
+                  tutorials, or user guides&mdash;your customers can achieve their
+                  goals and enjoy your products without needing to call support to get started.
+                </p>
+                <h2>User Experience Design</h2>
+                <p>
+                  I have a deep interest in understanding the different ways
+                  users discover and process information.{' '}
+                </p>
+                <p>
+                  I keep myself updated with the latest in Information
+                  Architecture and UX design through various meetups, books, and
+                  online resources. I am proficient at using popular software
+                  tools to design illustrations, graphics, workflows, and
+                  manipulate images to simplify technical information and
+                  enhance users’ understanding of the material.
+                </p>
+                <h2>Web Development</h2>
+                <p>
+                  In the ever-changing world of science and technology, I stay
+                  plugged-in to modern tools and technologies. I believe every
+                  technical communications professional must possess basic
+                  coding skills to produce content for various platforms. I
+                  continuously educate and train myself in various coding and
+                  programming languages.{' '}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default portfolioRoute
